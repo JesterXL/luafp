28500e0b4b41e1e4ca2b4ca7333f10f381888639
Jesse Warden <jesterxl@jessewarden.com>
2018-01-21 09:33:33 -0500

    found a mathematician who may have a working travis build

--- coverage.sh
+++ coverage.sh
@@ -3,13 +3,15 @@ rm luacov.report.out.index
 rm luacov.stats.out
 rm -rf ./coverage
 rm -rf coverage
-busted -c test
-luacov
+# busted test -c -p spec
+busted --verbose --coverage spec
+# luacov
 # luacov-console
 # luacov-console -s
+# luacov-console -l luafp
 
 
 # mkdir ./coverage
 # cp luacov.stats.out ./coverage/lcov.info
-luacov-coveralls -i luafp -t 97tePdW8ffhc1OED7xCnW2EuDd2nyCVQ3
+# luacov-coveralls -i luafp -t 97tePdW8ffhc1OED7xCnW2EuDd2nyCVQ3
 # istanbul report
